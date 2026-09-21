2ca0f0dcdcdbd63067009b044be699291a2bc2db
anshul-chaudhary-dev <anshulchaudhary3003@gmail.com>
2026-09-21 17:10:21 +0530

    Add files via upload

--- organizing_a_list.py
+++ organizing_a_list.py
@@ -1,6 +1,43 @@
 # Organizing a list 
 
 #sort() method
-cars = ['bmw','audi','toyota','subaro']
+cars = ['bmw','audi','toyota','subaru']
+print(cars)
 cars.sort()
 print(cars)
+
+
+# sort(reverce=true)
+
+print(cars)
+cars.sort(reverse=True)
+print(cars)
+
+
+#sorting temporarily with the sorted() function
+
+print(cars)
+print(sorted(cars))
+print(cars)
+
+
+
+# reverse order 
+
+print(cars)
+cars.reverse()
+print(cars)
+# Note: The revers () method change the order of a list permanently, but you can revert the original order by applying revers() method to the same list a second time.
+
+
+
+#finding the length of a list:
+    
+cars = ['bmw','audi','toyota','subaru']
+print(cars)
+print(len(cars))
+
+
+
+
+
