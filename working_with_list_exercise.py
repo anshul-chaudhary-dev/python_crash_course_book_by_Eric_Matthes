aa5f887a109bb55ec0a1d430620d6d280985d544
anshul-chaudhary-dev <anshulchaudhary3003@gmail.com>
2026-09-23 21:10:21 +0530

    Add files via upload

---
+++ working_with_list_exercise.py
@@ -0,0 +1,21 @@
+# Exercise 4.1 :
+    
+    
+pizza_stores = ['dominos','pizzahut','pappers']
+for pizza in pizza_stores:
+    print(f"I like {pizza.title()} peperoni pizza")
+    
+    
+print("peperoni pizza is very cheezy. \nfarmhouse pizza is very colorful. \nburger pizza is also very delicious. \nsuch as i really love pizza")
+
+# animal;
+
+animals = ['cat','dog','rat']
+for animal in animals :
+    print(animal.title())
+    
+animals = ['cat','dog','rat']
+for animal in animals :
+    print(f"such as A {animal.title()} would make a great pet.")
+
+print("\nAny of these animals would make a great pet.")
